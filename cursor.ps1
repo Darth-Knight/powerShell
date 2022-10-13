5aa30b7d37a01f7d2de79e66cf73b2288462d797
choudhary <rakshit.choudhary@grofers.com>
2022-10-13 21:45:15 +0530

    updated timediff and offset values

--- cursor.ps1
+++ cursor.ps1
@@ -3,11 +3,11 @@ Add-Type -AssemblyName System.Windows.Forms
 while ($true)
 {
   $offset = 500     # Pixels by which the cursor is to be moved
-  $timeDiff = 10    # Seconds after which we want the cursor to be moved - We can randomize this time as well
+  $timeDiff = 2    # Seconds after which we want the cursor to be moved - We can randomize this time as well
 
   $Pos = [System.Windows.Forms.Cursor]::Position
-  $horizontal = ($pos.X % $offset) + 1
-  $vertical = ($pos.Y % $offset) + 1
+  $horizontal = ($pos.X % $offset) + 500
+  $vertical = ($pos.Y % $offset) + 300
 
   [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($horizontal, $vertical)
 
